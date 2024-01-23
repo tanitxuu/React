9a96ea9cae5349a0936974b39fd2fc7c26427474
tania <taniavieco@gmail.com>
2024-01-23 14:40:03 +0100

    hoy

--- Clase/rutas/src/App.js
+++ Clase/rutas/src/App.js
@@ -1,48 +1,57 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component, useState} from 'react';
-import { Row, Col, Card, CardTitle, CardText, Form, FormGroup, Button, Label, Input } from 'reactstrap';
-
-function App(){
-  const lista = ['Estepona', 'Escañela', 'Vitoria', 'Madrid', 'Zaragoza', 'Jaen'];
+import React, { useState } from 'react';
+import { Row, Col, Card, CardText, Form, FormGroup,Label, Input } from 'reactstrap';
+function Menu(props) {
+  return (
+    <div>
+      <Row>
+        <Col sm="4"></Col>
+        <Col sm="4">
+          <Card body>
+            <Form inline>
+              <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+                <Label className="me-sm-2" for="exampleEmail">Buscar Ciudad</Label>
+                <Input
+                  placeholder="Ciudad"
+                  type="text"
+                  onChange={props.buscarciudad}
+                />
+              </FormGroup>
+              <Label>Resultados de Busqueda:</Label>
+              <CardText className="text-danger">
+                <ul>
+                  {props.ciudad.map((v, i) => <li key={i}>{v}</li>)}
+                </ul>
+              </CardText>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+function App() {
+  const lista = ['Estépona', 'Escañela', 'Vitoria', 'Madrid', 'Zaragoza', 'Jaen', 'Leon'];
   const [ciudad, setCiudad] = useState([]);
 
   const handleChange = (event) => {
-    const buscar= event.target.value.toLowerCase()
+    const buscar = event.target.value.toLowerCase()
     let bciudad = lista.filter(v => v.toLowerCase().startsWith(buscar));
     setCiudad(bciudad);
-    
+
   }
 
-    return (
-      <div className="App">
-      <Row>
-      <Col sm="4"></Col>
-      <Col sm="4">
-        <Card body>
-          <Form inline>
-            <FormGroup className="mb-2 me-sm-2 mb-sm-0">
-              <Label className="me-sm-2" for="exampleEmail">Buscar Ciudad</Label> 
-              <Input
-                placeholder="Ciudad"
-                type="text"
-                onChange={handleChange} 
-              />
-            </FormGroup>
-            <CardText className="text-danger">
-              <ul>
-                {ciudad.map((v,i)=><li key={i}>{v}</li>)}
-              </ul>
-            </CardText>
-          </Form>
-        </Card>
-      </Col>
-    </Row>
-
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Menu
+        buscarciudad={handleChange}
+        ciudad={ciudad}
+      />
+    </div>
+  );
+}
 
 
 export default App;
